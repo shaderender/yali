256531f163b9292a7d7ee7e8faa213bd7c1d1d9e
Michael Baxter <6093142+shaderender@users.noreply.github.com>
2022-11-25 17:14:24 -0800

    Improve ilox command; add basic formatter (incomplete)

--- interpreter/ilox/lox/lox.go
+++ interpreter/ilox/lox/lox.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"ilox/lox/loxerr"
 	"ilox/lox/scan"
+	"ilox/lox/token"
 	"log"
 	"os"
+	"strings"
 )
 
 type Lox struct {
@@ -18,19 +20,53 @@ func New() Lox {
 }
 
 func (l *Lox) Run(args []string) {
-	if len(args) > 1 {
-		fmt.Println("Usage: ilox [script]")
+	unrecognized := func() {
+		fmt.Fprintf(os.Stderr, "Unrecognized arguments: %q\n", strings.Join(args, " "))
+		fmt.Fprintf(os.Stderr, usage())
 		os.Exit(64)
-	} else if len(args) == 1 {
-		if err := l.runFile(args[0]); err != nil {
-			log.Fatal(err)
-		}
-	} else {
+	}
+
+	switch len(args) {
+	case 0:
+		// REPL
 		l.errRep.UseStdout = true
 		if err := l.runPrompt(); err != nil {
 			log.Fatal(err)
 		}
+	case 1:
+		// help or bad arg
+		if args[0] == "help" {
+			fmt.Println(usage())
+			os.Exit(0)
+		} else {
+			unrecognized()
+		}
+	case 2:
+		path := args[1]
+		switch args[0] {
+		case "run":
+			if err := l.runFile(path); err != nil {
+				log.Fatal(err)
+			}
+		case "format":
+			if err := l.runFormatter(path); err != nil {
+				log.Fatal(err)
+			}
+		default:
+			unrecognized()
+		}
+	default:
+		unrecognized()
 	}
+}
+
+func usage() string {
+	s := "Usage:\n"
+	s += "  ilox                        runs REPL\n"
+	s += "  ilox run [script]           runs the script\n"
+	s += "  ilox format [script]        formats the script\n"
+	s += "  ilox help\n"
+	return s
 }
 
 func (l *Lox) runFile(path string) error {
@@ -42,6 +78,25 @@ func (l *Lox) runFile(path string) error {
 	if l.errRep.HasError() {
 		os.Exit(65) // TODO: Should we really exit from here?
 	}
+	return nil
+}
+
+func (l *Lox) runFormatter(path string) error {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	scanner := scan.NewScanner(string(bytes), &l.errRep)
+	tokens := scanner.ScanTokens()
+
+	if l.errRep.HasError() {
+		os.Exit(65) // TODO: Should we use the same value as runFile?
+	}
+
+	src := token.Format(tokens)
+	fmt.Println(src)
+
 	return nil
 }
 
---
+++ interpreter/ilox/lox/token/formatter.go
@@ -0,0 +1,83 @@
+package token
+
+import (
+	"strings"
+)
+
+func Format(tokens []Token) string {
+	lines := []string{}
+
+	line := ""
+	prev := Newline
+	indent := 0
+	for _, token := range tokens {
+		switch token.t {
+		case LeftBrace:
+			line += " " + token.lexeme
+			// TODO: Consolodate below
+			line = nSpaces(indent) + strings.TrimSpace(line)
+			lines = append(lines, line)
+			line = ""
+			// TODO: Consolodate above
+			indent++
+		case Newline:
+			line = nSpaces(indent) + strings.TrimSpace(line)
+			lines = append(lines, line)
+			line = ""
+		case RightBrace:
+			indent--
+			fallthrough
+		default:
+			line += prefix(prev, token.t) + token.lexeme + suffix(prev, token.t)
+		}
+		prev = token.t
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+func prefix(prev, t TokenType) string {
+	switch t {
+	case Plus, Slash:
+		return " "
+	case Star:
+		return " "
+	case Equal, EqualEqual, Greater, GreaterEqual, Less, LessEqual:
+		return " "
+	case And, Or, If, Else:
+		return " "
+	case Comment:
+		if prev != Newline {
+			return " "
+		}
+	}
+	return ""
+}
+
+func suffix(prev, t TokenType) string {
+	switch t {
+	case Plus, Slash:
+		return " "
+	case Star:
+		return " "
+	case Equal, EqualEqual, Greater, GreaterEqual, Less, LessEqual:
+		return " "
+	case And, Or, If, Else:
+		return " "
+	case For, While:
+		return " "
+	case Var, Fun, Class, Print:
+		return " "
+	}
+	return ""
+}
+
+// TODO: Find a more efficient method.
+func nSpaces(n int) string {
+	n *= 4
+	s := ""
+	for i := 0; i < n; i++ {
+		s += " "
+	}
+	return s
+}
